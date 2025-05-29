810358962ef6fbc88bef56169e4cab96ef11e7ab
Avihu <avihuhenya@gmail.com>
2025-05-29 17:44:07 +0300

    update to simple-demo

--- ads/main.py
+++ ads/main.py
@@ -31,17 +31,6 @@ def getads():
     while True:
         try:
             with engine.connect() as conn:
-                if propagator is not None:
-                    # import opentelemetry.trace as trace
-                    # tracer = trace.get_tracer(instrumenting_module_name="database/sql")
-                    # with tracer.start_as_current_span("SELECT * FROM ads") as span:
-                        # span.set_attribute("db.type", "mysql")
-                        # span.set_attribute("db.instance", "adsdb")
-                        # span.set_attribute("db.statement", "SELECT * FROM ads'")
-                        result = conn.execute(sqlalchemy.text('SELECT * FROM ads'))
-                        ads = [dict(row) for row in result.mappings().all()]
-                        app.logger.info("Ads retrieved from the database: {}".format(ads))
-                        return ads
                 result = conn.execute(sqlalchemy.text('SELECT * FROM ads'))
                 ads = [dict(row) for row in result.mappings().all()]
                 app.logger.info("Ads retrieved from the database: {}".format(ads))
@@ -128,12 +117,8 @@ def main():
     PORT = int(os.getenv('PORT', '8080'))
 
     engine = create_engine("mysql+pymysql://adsuser:adspass@mysql.kv-mall-infra:3306/adsdb")
-    if propagator is not None:
-        app.logger.info("Using OpenTelemetry")
-        listener = BeforeExecuteFactory(with_opentelemetry=True)
-    else:
-        app.logger.info("Not using OpenTelemetry")
-        listener = BeforeExecuteFactory()
+    app.logger.info("Not using OpenTelemetry")
+    listener = BeforeExecuteFactory()
   
     event.listen(engine, 'before_cursor_execute', listener, retval=True)
     app.run(host='0.0.0.0', port=PORT, debug=False)
